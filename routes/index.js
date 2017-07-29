cf556d2fabffd79f984ca2680a4d0ab88a222cd7
James Le <le_j6@denison.edu>
2017-07-29 15:35:43 -0700

    Index File and View

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+module.exports = function(app, config) {
+  app.get('/', function(req, res) {
+    var Cosmic = require('cosmicjs')
+    Cosmic.getObjectType(config, { type_slug: 'crons' }, function(err, response) {
+      res.locals.crons = response.objects.all
+      res.locals.bucket_slug = config.bucket.slug
+      res.render('index.html')
+    })
+  })
+}
